7d09582d785afcbfd63017aadeea976247db55f8
RainierDotulong <rainierkd@gmail.com>
2022-07-22 14:15:16 -0700

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+	res.send('home page')
+})
+app.get('/club', (req, res) => {
+	res.send('welcome to the club')
+})
+
+app.listen(PORT, () => {
+	console.log(`listening to port${PORT}`)
+})
